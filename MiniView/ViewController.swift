30765539977e6195158557866589acb8099ad492
Laurentiu Ile <laurentiu.ile@fortech.ro>
2020-12-03 17:08:01 +0200

    Initial Commit

---
+++ MiniView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniView
+//
+//  Created by Laurentiu Ile on 03/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
